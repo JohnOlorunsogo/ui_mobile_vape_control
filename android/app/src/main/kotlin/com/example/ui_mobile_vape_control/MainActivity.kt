64bf568b69e068a1dd611cd0b4e220bcdaef95c4
John Olorunsogo <olorunsogojohn6@gmail.com>
2023-07-04 01:19:12 +0000

    neumorphic mobile ui design

---
+++ android/app/src/main/kotlin/com/example/ui_mobile_vape_control/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_mobile_vape_control
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Demo',
+
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
